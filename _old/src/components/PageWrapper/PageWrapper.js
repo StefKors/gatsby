547635c08938701f3804637f9ad63b42aa12204a
StefKors <stef.kors@gmail.com>
2021-01-22 19:41:31 +0100

    remove cruft

--- _old/src/components/PageWrapper/PageWrapper.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import styles from "./PageWrapper.module.css"
-
-
-const PageWrapper = props => <div className={styles.wrapper}>{props.children}</div>
-
-export default PageWrapper
