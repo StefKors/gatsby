621e8e48edf4820d42b0dad4036d48052925e6c3
Stef Kors <stef.kors@gmail.com>
2021-04-08 15:57:31 +0200

    add iframe 4

---
+++ pages/dev/iframe4.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>iframe in iframe</h1>
+    <iframe
+      width="100%"
+      height="300"
+      src="https://stefkors.github.io/iframe.html/"
+      allowfullscreen="allowfullscreen"
+      allowpaymentrequest
+      frameborder="0"
+    ></iframe>
+  </div>
+</template>
