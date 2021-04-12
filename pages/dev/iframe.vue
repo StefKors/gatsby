ec0a2ac6f6985167c1a736452dde4e1463792461
Stef Kors <stef.kors@gmail.com>
2021-04-12 17:33:43 +0200

    add content

--- pages/dev/iframe.vue
+++ pages/dev/iframe.vue
@@ -1,3 +1,193 @@
 <template>
-  <iframe width="100%" height="300" src="https://stefkors.github.io/index.html/" allowfullscreen="allowfullscreen" allowpaymentrequest frameborder="0"></iframe>
+  <div>
+    <iframe
+      width="100%"
+      height="300"
+      src="https://stefkors.github.io/index.html/"
+      allowfullscreen="allowfullscreen"
+      allowpaymentrequest
+      frameborder="0"
+    ></iframe>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
+      voluptatibus nisi quos nobis delectus ad aspernatur. Ipsa, velit quisquam
+      quaerat dolores distinctio expedita laudantium rerum, facilis aliquid,
+      aperiam eveniet libero. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Itaque delectus perferendis cumque, eaque repellat
+      temporibus quis fuga perspiciatis ex hic magnam animi provident sit quam
+      amet, vitae veritatis id nemo! Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Pariatur obcaecati, explicabo esse consequatur tenetur
+      nesciunt tempore et quaerat. Earum a numquam molestiae quasi quidem
+      delectus blanditiis suscipit architecto voluptate accusantium!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
+      voluptatibus nisi quos nobis delectus ad aspernatur. Ipsa, velit quisquam
+      quaerat dolores distinctio expedita laudantium rerum, facilis aliquid,
+      aperiam eveniet libero. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Itaque delectus perferendis cumque, eaque repellat
+      temporibus quis fuga perspiciatis ex hic magnam animi provident sit quam
+      amet, vitae veritatis id nemo! Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Pariatur obcaecati, explicabo esse consequatur tenetur
+      nesciunt tempore et quaerat. Earum a numquam molestiae quasi quidem
+      delectus blanditiis suscipit architecto voluptate accusantium!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
+      voluptatibus nisi quos nobis delectus ad aspernatur. Ipsa, velit quisquam
+      quaerat dolores distinctio expedita laudantium rerum, facilis aliquid,
+      aperiam eveniet libero. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Itaque delectus perferendis cumque, eaque repellat
+      temporibus quis fuga perspiciatis ex hic magnam animi provident sit quam
+      amet, vitae veritatis id nemo! Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Pariatur obcaecati, explicabo esse consequatur tenetur
+      nesciunt tempore et quaerat. Earum a numquam molestiae quasi quidem
+      delectus blanditiis suscipit architecto voluptate accusantium!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
+      voluptatibus nisi quos nobis delectus ad aspernatur. Ipsa, velit quisquam
+      quaerat dolores distinctio expedita laudantium rerum, facilis aliquid,
+      aperiam eveniet libero. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Itaque delectus perferendis cumque, eaque repellat
+      temporibus quis fuga perspiciatis ex hic magnam animi provident sit quam
+      amet, vitae veritatis id nemo! Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Pariatur obcaecati, explicabo esse consequatur tenetur
+      nesciunt tempore et quaerat. Earum a numquam molestiae quasi quidem
+      delectus blanditiis suscipit architecto voluptate accusantium!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
+      voluptatibus nisi quos nobis delectus ad aspernatur. Ipsa, velit quisquam
+      quaerat dolores distinctio expedita laudantium rerum, facilis aliquid,
+      aperiam eveniet libero. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Itaque delectus perferendis cumque, eaque repellat
+      temporibus quis fuga perspiciatis ex hic magnam animi provident sit quam
+      amet, vitae veritatis id nemo! Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Pariatur obcaecati, explicabo esse consequatur tenetur
+      nesciunt tempore et quaerat. Earum a numquam molestiae quasi quidem
+      delectus blanditiis suscipit architecto voluptate accusantium!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
+      voluptatibus nisi quos nobis delectus ad aspernatur. Ipsa, velit quisquam
+      quaerat dolores distinctio expedita laudantium rerum, facilis aliquid,
+      aperiam eveniet libero. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Itaque delectus perferendis cumque, eaque repellat
+      temporibus quis fuga perspiciatis ex hic magnam animi provident sit quam
+      amet, vitae veritatis id nemo! Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Pariatur obcaecati, explicabo esse consequatur tenetur
+      nesciunt tempore et quaerat. Earum a numquam molestiae quasi quidem
+      delectus blanditiis suscipit architecto voluptate accusantium!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
+      voluptatibus nisi quos nobis delectus ad aspernatur. Ipsa, velit quisquam
+      quaerat dolores distinctio expedita laudantium rerum, facilis aliquid,
+      aperiam eveniet libero. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Itaque delectus perferendis cumque, eaque repellat
+      temporibus quis fuga perspiciatis ex hic magnam animi provident sit quam
+      amet, vitae veritatis id nemo! Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Pariatur obcaecati, explicabo esse consequatur tenetur
+      nesciunt tempore et quaerat. Earum a numquam molestiae quasi quidem
+      delectus blanditiis suscipit architecto voluptate accusantium!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
+      voluptatibus nisi quos nobis delectus ad aspernatur. Ipsa, velit quisquam
+      quaerat dolores distinctio expedita laudantium rerum, facilis aliquid,
+      aperiam eveniet libero. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Itaque delectus perferendis cumque, eaque repellat
+      temporibus quis fuga perspiciatis ex hic magnam animi provident sit quam
+      amet, vitae veritatis id nemo! Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Pariatur obcaecati, explicabo esse consequatur tenetur
+      nesciunt tempore et quaerat. Earum a numquam molestiae quasi quidem
+      delectus blanditiis suscipit architecto voluptate accusantium!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
+      voluptatibus nisi quos nobis delectus ad aspernatur. Ipsa, velit quisquam
+      quaerat dolores distinctio expedita laudantium rerum, facilis aliquid,
+      aperiam eveniet libero. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Itaque delectus perferendis cumque, eaque repellat
+      temporibus quis fuga perspiciatis ex hic magnam animi provident sit quam
+      amet, vitae veritatis id nemo! Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Pariatur obcaecati, explicabo esse consequatur tenetur
+      nesciunt tempore et quaerat. Earum a numquam molestiae quasi quidem
+      delectus blanditiis suscipit architecto voluptate accusantium!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
+      voluptatibus nisi quos nobis delectus ad aspernatur. Ipsa, velit quisquam
+      quaerat dolores distinctio expedita laudantium rerum, facilis aliquid,
+      aperiam eveniet libero. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Itaque delectus perferendis cumque, eaque repellat
+      temporibus quis fuga perspiciatis ex hic magnam animi provident sit quam
+      amet, vitae veritatis id nemo! Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Pariatur obcaecati, explicabo esse consequatur tenetur
+      nesciunt tempore et quaerat. Earum a numquam molestiae quasi quidem
+      delectus blanditiis suscipit architecto voluptate accusantium!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
+      voluptatibus nisi quos nobis delectus ad aspernatur. Ipsa, velit quisquam
+      quaerat dolores distinctio expedita laudantium rerum, facilis aliquid,
+      aperiam eveniet libero. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Itaque delectus perferendis cumque, eaque repellat
+      temporibus quis fuga perspiciatis ex hic magnam animi provident sit quam
+      amet, vitae veritatis id nemo! Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Pariatur obcaecati, explicabo esse consequatur tenetur
+      nesciunt tempore et quaerat. Earum a numquam molestiae quasi quidem
+      delectus blanditiis suscipit architecto voluptate accusantium!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
+      voluptatibus nisi quos nobis delectus ad aspernatur. Ipsa, velit quisquam
+      quaerat dolores distinctio expedita laudantium rerum, facilis aliquid,
+      aperiam eveniet libero. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Itaque delectus perferendis cumque, eaque repellat
+      temporibus quis fuga perspiciatis ex hic magnam animi provident sit quam
+      amet, vitae veritatis id nemo! Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Pariatur obcaecati, explicabo esse consequatur tenetur
+      nesciunt tempore et quaerat. Earum a numquam molestiae quasi quidem
+      delectus blanditiis suscipit architecto voluptate accusantium!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
+      voluptatibus nisi quos nobis delectus ad aspernatur. Ipsa, velit quisquam
+      quaerat dolores distinctio expedita laudantium rerum, facilis aliquid,
+      aperiam eveniet libero. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Itaque delectus perferendis cumque, eaque repellat
+      temporibus quis fuga perspiciatis ex hic magnam animi provident sit quam
+      amet, vitae veritatis id nemo! Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Pariatur obcaecati, explicabo esse consequatur tenetur
+      nesciunt tempore et quaerat. Earum a numquam molestiae quasi quidem
+      delectus blanditiis suscipit architecto voluptate accusantium!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
+      voluptatibus nisi quos nobis delectus ad aspernatur. Ipsa, velit quisquam
+      quaerat dolores distinctio expedita laudantium rerum, facilis aliquid,
+      aperiam eveniet libero. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Itaque delectus perferendis cumque, eaque repellat
+      temporibus quis fuga perspiciatis ex hic magnam animi provident sit quam
+      amet, vitae veritatis id nemo! Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Pariatur obcaecati, explicabo esse consequatur tenetur
+      nesciunt tempore et quaerat. Earum a numquam molestiae quasi quidem
+      delectus blanditiis suscipit architecto voluptate accusantium!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
+      voluptatibus nisi quos nobis delectus ad aspernatur. Ipsa, velit quisquam
+      quaerat dolores distinctio expedita laudantium rerum, facilis aliquid,
+      aperiam eveniet libero. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Itaque delectus perferendis cumque, eaque repellat
+      temporibus quis fuga perspiciatis ex hic magnam animi provident sit quam
+      amet, vitae veritatis id nemo! Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Pariatur obcaecati, explicabo esse consequatur tenetur
+      nesciunt tempore et quaerat. Earum a numquam molestiae quasi quidem
+      delectus blanditiis suscipit architecto voluptate accusantium!
+    </p>
+  </div>
 </template>
